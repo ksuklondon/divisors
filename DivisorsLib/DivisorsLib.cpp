e04cc0df51953afe1668a34a419e64aea3462208
ksuklondon <staniszewskakalina@gmail.com>
2024-11-08 17:42:51 +0000

    Tasks completed

--- DivisorsLib/DivisorsLib.cpp
+++ DivisorsLib/DivisorsLib.cpp
@@ -1,13 +1,33 @@
 #include <vector>
 #include "Divisors.h"
 
-std::vector<unsigned long long> divisors(unsigned long long number)
-{
-	std::vector<unsigned long long> result;
-	for (unsigned long long i = 1; i <= number; ++i)
-	{
-		if (number % i == 0)
-			result.push_back(i);
-	}
-	return result;
+#include <algorithm>
+#include <cmath> // For sqrt function
+
+std::vector<unsigned long long> divisors(unsigned long long number) {
+    std::vector<unsigned long long> result;
+
+    for (unsigned long long i = 1; i <= sqrt(number); i++) {
+        if (number % i == 0) {
+            result.push_back(i);
+
+            if (number / i != i) {
+                result.push_back(number / i);
+            }
+        }
+    }
+
+    std::sort(result.begin(), result.end());
+    return result;
 }
+
+//std::vector<unsigned long long> divisors(unsigned long long number)
+//{
+//	std::vector<unsigned long long> result;
+//	for (unsigned long long i = 1; i <= number; ++i)
+//	{
+//		if (number % i == 0)
+//			result.push_back(i);
+//	}
+//	return result;
+//}
